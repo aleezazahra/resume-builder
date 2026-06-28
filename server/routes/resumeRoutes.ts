fb64a008af658ccbda87ea498c0ea7abda006a62
aleezazahra <azahradum@gmail.com>
2026-06-28 18:32:13 +0500

    fix

--- server/routes/resumeRoutes.ts
+++ server/routes/resumeRoutes.ts
@@ -15,7 +15,7 @@ const resumeRouter = express.Router();
 
 resumeRouter.post("/create", protect, createResume);
 resumeRouter.get("/list", protect, getUserResumes);
-resumeRouter.get("/get/:resumeId", protect, getResumeById);
+resumeRouter.get("/get/:resumeId",  getResumeById);
 resumeRouter.put("/update", protect, upload.single("image"), updateResume);
 resumeRouter.delete("/delete/:resumeId", protect, deleteResume);
 
