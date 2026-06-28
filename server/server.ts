d79875fee14d56e951b4580c167dab39fa1773f7
aleezazahra <azahradum@gmail.com>
2026-06-28 17:14:53 +0500

    fix

--- server/server.ts
+++ server/server.ts
@@ -15,7 +15,8 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://resume-builder-1v7l.vercel.app"
+    "https://resume-builder-1v7l.vercel.app",
+    "https://resume-builder-pied-mu.vercel.app"
   ],
   credentials: true
 }));
