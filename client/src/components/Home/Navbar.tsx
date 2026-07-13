2adbefae6b3b9af62843f6fd1f5d6037f0b684bd
aleezazahra <azahradum@gmail.com>
2026-07-14 00:33:05 +0500

    created the logout button

--- client/src/components/Home/Navbar.tsx
+++ client/src/components/Home/Navbar.tsx
@@ -1,13 +1,21 @@
 import { Link, useNavigate } from "react-router-dom";
 import Logo from "../../assets/logo3.png";
-import { ArrowLeft, Github } from "lucide-react";
-import { Moon,Sun } from "lucide-react";
+import { ArrowLeft, Github, LogOut } from "lucide-react";
+import { useDispatch } from "react-redux";
+import { logout } from "../../app/features/authSlice";
+
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    dispatch(logout());
+    navigate("/login");
+  };
 
   return (
-
     <header className="w-full border-b border-white/10 bg-black/20 backdrop-blur-2xl">
       <nav className="flex h-20 w-full items-center justify-between px-6 md:px-10 lg:px-16">
         <Link to="/">
@@ -21,21 +29,26 @@ const Navbar = () => {
         <div className="flex items-center gap-3">
           <button
             onClick={() => navigate("/")}
-            className="flex btn items-center justify-center rounded-full border  text-white transition-all duration-300 hover:border-white/20 0"
+            className="flex btn items-center justify-center rounded-full border text-white transition-all duration-300 hover:border-white/20"
           >
             <ArrowLeft size={18} />
           </button>
-
-       
-
-          <a
+      <a
+          
             href="https://github.com/aleezazahra/resume-builder"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex btn items-center justify-center rounded-full border  text-white transition-all duration-300 hover:border-white/20 "
+            className="flex btn items-center justify-center rounded-full border text-white transition-all duration-300 hover:border-white/20"
           >
             <Github size={18} />
           </a>
+
+          <button
+            onClick={handleLogout}
+            className="flex btn items-center justify-center rounded-full border text-white transition-all duration-300 hover:border-white/20"
+          >
+            <LogOut size={18} />
+          </button>
         </div>
       </nav>
     </header>
